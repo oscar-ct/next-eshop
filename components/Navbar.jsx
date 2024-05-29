527b81911452bd4cbd428fde990bd9aa938cd06a
Oscar Castro <oscar.a.castro818@gmail.com>
2024-05-29 18:52:06 -0500

    feat: setup session status var for nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,30 @@
 "use client";
-import {useState} from "react";
+
+import {useEffect, useState} from "react";
 import {FaUser, FaChevronDown, FaSearch} from "react-icons/fa";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+// import {getServerSession} from "next-auth";
 
 
 const Navbar = () => {
+
+    const { data: session } = useSession();
+    // const session = await getServerSession();
+    console.log(session)
+
     return (
         <div>
-            Navbar
+            {
+                session && (
+                    <span onClick={() => signOut()}>Logout</span>
+                )
+            }
+            {
+                !session && (
+                    <Link href={"/login"}>Login</Link>
+                )
+            }
         </div>
     );
 };
