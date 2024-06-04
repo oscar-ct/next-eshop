f91a212a1220172597edf791ba1d647387c4a890
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-04 01:38:00 -0500

    move GET products route to new route with params for data querying, update controller function

--- app/api/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/route.js
+++ app/api/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/route.js
@@ -1,45 +1,50 @@
 import connectDB from "@/config/db";
 import Product from "@/models/Product";
-import { parse } from "url";
 
-// helper function
+
 const capitalizeFirstChar = (string) => {
     return string.substring(0, 1).toUpperCase() + string.substring(1, string.length);
 };
 
-//  GET /api/products
+//  GET /api/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]
+
 export const GET = async (req, {params}) => {
-    console.log(params)
+    const { sortTerm, categoryTerm, pageNumber } = params;
+
     try {
-        await connectDB();
-        const { query } = parse(req.url, true);
         let products;
-        // set number of products per page, search or sort queries will return 16 products, else only 8 (i.e. homepage)
-        const pageSize = query.searchTerm || query.sortByTerm ? 16 : 14;
-        // if page number exists from url search params, set page number, else set to 1
-        const page = Number(query.pageNumber) || 1;
-        // if url search params includes a search or sort or category parameter, set keyword/categoryTerm to that, else leave blank, this is used to update the title of the search or category page
-        const keyword = query.searchTerm || query.sortByTerm || "";
-        const categoryTerm = query.filterTerm || "";
-        // if url sort param exists, sort by that params, else default to sort by newest to oldest
-        const sortTerm = query.sortByTerm === "toprated" ? {rating: -1} : query.sortByTerm === "latest" ? {createdAt: -1} : query.sortByTerm === "price-asc" ? {price: +1} : query.sortByTerm === "price-dsc" ? {price: -1} : {createdAt: -1};
-        // if url search param exists, filter by that search keyword param, else default to all i.e. {}
-        const searchTerm = query.searchTerm ? { name: {$regex: query.searchTerm, $options: "i"}, isDisabled: false} : query.sortByTerm === "toprated" ? {rating: {$gt: 0}, isDisabled: false} : {isDisabled: false};
-        // initialize count variable
+        const sortTermString = sortTerm || "";
+        const categoryTermString = categoryTerm || "";
+
+        const page = pageNumber || 1;
+        const pageSize = 6;
+
+        const validQueries = ["toprated", "latest", "price-asc","price-dec"];
+
+        if (!validQueries.includes(sortTerm)) {
+            return Response.json({
+                categoryTermString,
+                page,
+                pages: 0,
+                sortTermString,
+                products: []
+            });
+        }
+        await connectDB();
+        const sortTermQuery = sortTerm === "toprated" ? {rating: -1} : sortTerm === "latest" ? {createdAt: -1} : sortTerm === "price-asc" ? {price: +1} : sortTerm === "price-dsc" ? {price: -1} : {createdAt: -1};
+        const filterTerm = sortTerm === "toprated" ? {rating: {$gt: 0}, isDisabled: false} : {isDisabled: false};
+
         let count;
-        // if category exists and category term does not equal all do this
-        if (query.filterTerm && query.filterTerm !== "all") {
-            // set count to the numbers of results found based on category parameter
+        if (categoryTerm && categoryTerm !== "all") {
             count = await Product
-                .countDocuments({...searchTerm})
+                .countDocuments({...filterTerm})
                 .where("category")
-                .equals(capitalizeFirstChar(req.query.filterTerm));
-            // set products to query category parameter
+                .equals(capitalizeFirstChar(categoryTerm));
             products = await Product
-                .find({...searchTerm})
+                .find({...filterTerm})
                 .where("category")
-                .equals(capitalizeFirstChar(req.query.filterTerm))
-                .sort({...sortTerm})
+                .equals(capitalizeFirstChar(categoryTerm))
+                .sort({...sortTermQuery})
                 .limit(pageSize)
                 .skip(pageSize * (page-1));
             // return JSON to api call
@@ -48,17 +53,16 @@ export const GET = async (req, {params}) => {
                     products,
                     page,
                     pages: Math.ceil(count / pageSize),
-                    keyword: keyword,
-                    categoryTerm: categoryTerm
+                    sortTermString,
+                    categoryTermString,
                 }
             );
         }
-        // set count to the numbers of results found based on category parameter
-        count = await Product.countDocuments({...searchTerm});
-        // set products to query search parameter
+
+        count = await Product.countDocuments({...filterTerm});
         products = await Product
-            .find({...searchTerm})
-            .sort({...sortTerm})
+            .find({...filterTerm})
+            .sort({...sortTermQuery})
             .limit(pageSize)
             .skip(pageSize * (page-1));
         return Response.json(
@@ -66,7 +70,8 @@ export const GET = async (req, {params}) => {
                 products,
                 page,
                 pages: Math.ceil(count / pageSize),
-                keyword: keyword, categoryTerm: categoryTerm
+                sortTermString,
+                categoryTermString,
             }
         );
     } catch (e) {
