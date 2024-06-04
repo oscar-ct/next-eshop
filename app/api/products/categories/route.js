d257b296b81384121f862096d7e059f32c0f60c8
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-04 03:59:09 -0500

    create new GET endpoint route and controller function for product categories

---
+++ app/api/products/categories/route.js
@@ -0,0 +1,21 @@
+import connectDB from "@/config/db";
+import Product from "@/models/Product";
+
+
+//  GET /api/products/categories
+
+export const GET = async (req) => {
+    let products = [];
+    try {
+        await connectDB();
+        const categories = await Product.distinct("category").sort();
+        for (let i = 0; i < categories.length; i++) {
+            const product = await Product.where("category").equals(categories[i]).sort({rating: -1}).limit(1).select("images category");
+            products.push(product[0]);
+        }
+        return Response.json(products);
+    } catch (e) {
+        console.log(e);
+        return new Response("Something went wrong...", {status: 500});
+    }
+};
