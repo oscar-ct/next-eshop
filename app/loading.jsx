1a2b24ba7a1ceed3cb46ca610045378af218a712
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-05 03:05:34 -0500

    add custom calc hheight to loading page

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,7 @@
 
 const Loading = () => {
     return (
-        <div className={"z-30 w-full h-screen flex items-center justify-center"}>
+        <div className={"z-30 w-full mx-auto min-h-[calc(100vh-292px)] md:min-h-[calc(100vh-224px)] flex items-center justify-center"}>
             <span className="loading loading-bars loading-lg"/>
         </div>
     );
