cb1f3657b3e5431d633d26412c9baad7da70c050
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-07 16:22:43 -0500

    update products component title for mobile

--- components/HomePageProducts.jsx
+++ components/HomePageProducts.jsx
@@ -3,15 +3,18 @@ import ProductItem from "@/components/ProductItem";
 const HomePageProducts = ({ products, windowInnerWidth }) => {
     return (
         <div className={`pb-1 md:px-3 md:pb-10 pt-14 md:pt-0 bg-black/90 md:bg-transparent`}>
-            <div className={"h-12 md:bg-zinc-700"}>
+            <div className={"lg:h-12 md:bg-zinc-700"}>
                 <div className={"flex justify-center items-center h-full w-full"}>
-                    <h2 className={"lg:pl-3 text-3xl md:text-2xl text-white ibmplex"}>
+                    <h2 className={"hidden lg:block pl-3 text-2xl text-white"}>
+                        Latest Products
+                    </h2>
+                    <h2 className={"text-3xl font-bold text-white px-5"}>
                         Latest Products
                     </h2>
                 </div>
             </div>
-            <div className={"md:bg-white pt-10 md:border md:py-8 w-full flex flex-wrap justify-center"}>
-                {
+            <div className={"md:bg-white pt-14 lg:pt-10 md:border md:py-8 w-full flex flex-wrap justify-center"}>
+            {
                     products.products.map(function (product) {
                         return <ProductItem key={product._id} product={product} windowInnerWidth={windowInnerWidth}/>
                     })
