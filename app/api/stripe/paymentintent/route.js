441a9da98073cc70e06f99ffaa80dd11cab13869
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-10 05:41:31 -0500

    remove dollar to cent conversion

--- app/api/stripe/paymentintent/route.js
+++ app/api/stripe/paymentintent/route.js
@@ -5,18 +5,18 @@ import jwt from "jsonwebtoken";
 
 const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY);
 
-const calculateOrderAmountInCents = (dollarAmount) => {
-    dollarAmount = (dollarAmount + '').replace(/[^\d.-]/g, '');
-    if (dollarAmount && dollarAmount.includes('.')) {
-        dollarAmount = dollarAmount.substring(0, dollarAmount.indexOf('.') + 3);
-    }
-    return dollarAmount ? Math.round(parseFloat(dollarAmount) * 100) : 0;
-};
+// const calculateOrderAmountInCents = (dollarAmount) => {
+//     dollarAmount = (dollarAmount + '').replace(/[^\d.-]/g, '');
+//     if (dollarAmount && dollarAmount.includes('.')) {
+//         dollarAmount = dollarAmount.substring(0, dollarAmount.indexOf('.') + 3);
+//     }
+//     return dollarAmount ? Math.round(parseFloat(dollarAmount) * 100) : 0;
+// };
 
 export async function POST(req) {
     const { totalPriceFromBackend } = await req.json();
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmountInCents(totalPriceFromBackend),
+        amount: totalPriceFromBackend,
         currency: "usd",
         automatic_payment_methods: {
             enabled: true,
