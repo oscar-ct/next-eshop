bbac0d90db58208d7c65598b44ac35fa7515cc21
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-16 18:41:00 -0500

    increase search bar left padding

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -30,9 +30,9 @@ const CartIcon = ({ isValidShippingAddress, isValidPaymentMethod, totalCartItems
                     <div
                         onMouseEnter={() => setCartDropdownActive(true)}
                         onMouseLeave={() => setCartDropdownActive(false)}
-                        className="dropdown dropdown-end py-4"
+                        className="dropdown dropdown-end py-2.5"
                     >
-                        <Link href={"/cart"} className="px-2 h-[3rem] flex items-center">
+                        <Link href={"/cart"} className="pl-4 pr-2 h-[3rem] flex items-center">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                 {
@@ -42,11 +42,11 @@ const CartIcon = ({ isValidShippingAddress, isValidPaymentMethod, totalCartItems
                                 }
 
                             </div>
-                            <span className={"hidden lg:block normal-case pl-1"}>Cart</span>
+                            <span className={"hidden lg:block normal-case pl-1 text-sm"}>Cart</span>
                         </Link>
                         {
                             cartDropdownActive && (
-                                <div className="mt-4 z-10 absolute right-0 origin-top-right w-52 bg-neutral/70 shadow rounded-b-md">
+                                <div className="mt-2.5 z-10 absolute right-0 origin-top-right w-52 bg-neutral/70 shadow rounded-b-md">
                                     <div className="p-5">
                                         {
                                             totalCartItems !== 0 ? (
