b8d9fd3da4831a0d3b3a703b3b5ef3519cbf59b9
Oscar Castro <oscar.a.castro818@gmail.com>
2024-06-21 05:17:09 -0500

    covert cancel order and cancel item controllers to update order data from postgres db with prisma

--- app/api/orders/[id]/cancel/route.js
+++ app/api/orders/[id]/cancel/route.js
@@ -1,50 +1,44 @@
-import Order from "@/models/Order";
-import connectDB from "@/config/db";
+import prisma from "@/lib/prisma";
 
 //  DELETE /api/orders/[id]/cancel
 
 export const DELETE = async (req, {params}) => {
-    await connectDB();
     try {
-        const order = await Order.findById(params.id);
+        const order = await prisma.order.findFirst({
+            where : {
+                id: params.id
+            }
+        });
         if (!order) return new Response("Order not found...", {status: 404});
-
-        const { isShipped, canceledItems, orderItems } = order;
-        if (!isShipped) {
-            order.isCanceled = true;
-            order.canceledAt = Date.now();
-            if (canceledItems.length > 0) {
-                orderItems.forEach(function (item) {
-                    if (!item.isCanceled) {
-                        const data = {
-                            productId: item.productId,
-                            productPrice: item.price,
-                            productQuantity: item.quantity,
-                            canceledAt: Date.now(),
+        if (!order.isShipped) {
+            const updatedOrder = await prisma.order.update({
+                where: {
+                    id: params.id
+                },
+                data: {
+                    isCanceled: true,
+                    canceledAt: new Date(),
+                    itemsPrice: !order.isPaid ? 0 : order.itemsPrice,
+                    shippingPrice: !order.isPaid ? 0 : order.shippingPrice,
+                    taxPrice: !order.isPaid ? 0 : order.taxPrice,
+                    totalPrice: !order.isPaid ? 0 : order.totalPrice,
+                    orderItems : {
+                        updateMany: {
+                            where: {
+                                isCanceled: false
+                            },
+                            data: {
+                                isCanceled: true,
+                                canceledAt: new Date()
+                            }
                         }
-                        canceledItems.push(data);
                     }
-                    item.isCanceled = true;
-                });
-            } else {
-                orderItems.forEach(function (item) {
-                    item.isCanceled = true;
-                    const data = {
-                        productId: item.productId.toString(),
-                        productPrice: item.price,
-                        productQuantity: item.quantity,
-                        canceledAt: Date.now(),
-                    }
-                    canceledItems.push(data);
-                });
-            }
-            if (!order.isPaid) {
-                order.itemsPrice = 0;
-                order.shippingPrice = 0;
-                order.taxPrice = 0;
-                order.totalPrice = 0;
-            }
-            const updatedOrder = await order.save();
+                },
+                include: {
+                    orderItems: true,
+                    orderPayment: true,
+                }
+            });
             return Response.json(updatedOrder);
         } else {
             return new Response("This order cannot be canceled...", {status: 403});
