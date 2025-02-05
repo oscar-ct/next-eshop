5b84a6c42a711fad96def7fff86a47fb8beb5447
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-05 11:04:25 -0600

    add use effect hook to products component for fetching products data

--- components/HomePageProducts.jsx
+++ components/HomePageProducts.jsx
@@ -1,25 +1,58 @@
-import ProductItem from "@/components/ProductItem";
+"use client";
 
-const HomePageProducts = ({ products, windowInnerWidth }) => {
+import ProductItem from "@/components/ProductItem";
+import {useEffect, useState} from "react";
+import {fetchProducts} from "@/utils/api-requests/fetchRequests";
+
+const HomePageProducts = () => {
+
+    const [products, setProducts] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [windowInnerWidth, setWindowInnerWidth] = useState(typeof window !== "undefined" && window.innerWidth);
+
+    useEffect( () => {
+        const adjustWidth = () => setWindowInnerWidth(window.innerWidth);
+        window.addEventListener("resize", adjustWidth);
+        return () => window.removeEventListener("resize", adjustWidth);
+    });
+
+    useEffect(() => {
+        const fetchProductsData = async () => {
+            try {
+                const products = await fetchProducts();
+                setProducts(products);
+            } catch (e) {
+                console.log(e);
+            } finally {
+                setLoading(false);
+            }
+        };
+        if (products === null) fetchProductsData();
+    }, [products]);
+
+
     return (
-        <div className={`pb-1 md:px-3 md:pb-10 pt-14 md:pt-0 bg-black dark:bg-black md:bg-transparent 2xl:container 2xl:mx-auto`}>
-            <div className={"h-12 md:bg-zinc-700"}>
-                <div className={"flex justify-center items-center h-full w-full"}>
-                    <h2 className={"hidden lg:block pl-3 font-bold text-2xl text-white"}>
-                        Latest Products
-                    </h2>
-                    <h2 className={"lg:hidden text-3xl font-bold text-white px-5"}>
-                        Latest Products
-                    </h2>
-                </div>
+        <div className={"pb-14 rounded-2xl bg-zinc-50 sm:bg-white sm:shadow-lg dark:bg-slate-800"}>
+            <div className={"py-10 flex justify-center items-center h-full w-full md:py-12"}>
+                <h2 className={"font-bold text-3xl dark:text-white"}>
+                    Latest Products
+                </h2>
             </div>
-            <div className={"bg-black dark:bg-black md:bg-white pt-14 lg:pt-10 md:border md:py-8 w-full flex flex-wrap justify-center"}>
             {
-                    products.products.map(function (product) {
-                        return <ProductItem key={product.id} product={product} windowInnerWidth={windowInnerWidth}/>
-                    })
-                }
-            </div>
+                !products && loading ? (
+                    <div className={"h-64 flex items-center justify-center"}>
+                        <span className="loading loading-bars loading-lg"/>
+                    </div>
+                ) : (
+                    <div className={"w-full flex flex-wrap justify-center"}>
+                        {
+                            products.products.map(function (product) {
+                                return <ProductItem key={product.id} product={product} windowInnerWidth={windowInnerWidth}/>
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 };
