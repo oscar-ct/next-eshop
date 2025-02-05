04c406dff54a218c2cad7faf5818258c03a44522
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-05 11:15:55 -0600

    Clean up main home page component, add new modal component, moved data fetching to individual components

--- app/page.jsx
+++ app/page.jsx
@@ -1,119 +1,26 @@
-"use client";
+"use client"
 
 import HomePageProducts from "@/components/HomePageProducts";
 import HomePageIntro from "@/components/HomePageIntro";
-import {useEffect, useState} from "react";
-import {motion} from "framer-motion";
-import Loading from "@/app/loading";
-import {fetchProductCategories, fetchProducts} from "@/utils/api-requests/fetchRequests";
-import CustomBtn from "@/components/CustomBtn";
-import Image from "next/image";
-
+import OscarModal from "@/components/modals/OscarModal";
 
 
 const Page = () => {
 
-    const [products, setProducts] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [productsCategory, setProductsCategory] = useState(null);
-    const [loadingCategory, setLoadingCategory] = useState(true);
-    const [windowInnerWidth, setWindowInnerWidth] = useState(typeof window !== "undefined" && window.innerWidth);
+    return (
+        <>
+            <div className={"pb-10 relative overflow-hidden px-2 lg:px-4 2xl:px-0"}>
+                <div className={"z-10 m-auto max-w-screen-2xl flex flex-col relative sm:gap-12"}>
+                    <HomePageIntro/>
+                    {/*HomePageIntro includes: swiper slide, product categories*/}
+                    <HomePageProducts/>
+                </div>
+                <div className={"z-0 scale-125 -rotate-6 m-auto absolute w-full top-96 h-72 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"}/>
+            </div>
+            <OscarModal/>
+        </>
+    )
+}
 
-
-    useEffect(() => {
-        const fetchProductsData = async () => {
-            try {
-                const products = await fetchProducts();
-                setProducts(products);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                setLoading(false);
-            }
-        };
-        if (products === null) fetchProductsData();
-    }, [products]);
-
-    useEffect(() => {
-        const fetchProductsCategoryData = async () => {
-            try {
-                const products = await fetchProductCategories();
-                setProductsCategory(products);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                setLoadingCategory(false);
-            }
-        };
-        if (productsCategory === null) fetchProductsCategoryData();
-    }, [productsCategory]);
-
-
-    useEffect( () => {
-        const adjustWidth = () => setWindowInnerWidth(window.innerWidth);
-        window.addEventListener("resize", adjustWidth);
-        return () => window.removeEventListener("resize", adjustWidth);
-    });
-
-    if (!loading && products && !loadingCategory && productsCategory) {
-        return (
-            <>
-                <motion.div
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{opacity: 0}}
-                >
-                    <HomePageIntro windowInnerWidth={windowInnerWidth} productsCategory={productsCategory}/>
-                    <HomePageProducts products={products} windowInnerWidth={windowInnerWidth}/>
-                </motion.div>
-                <dialog id="oscar_modal" className="modal">
-                    <div className="modal-box">
-                        <div
-                            className={"w-full h-[28em] flex flex-col items-center justify-evenly"}>
-                            <div className={"pt-3 w-full flex flex-col justify-center items-center"}>
-                                <div
-                                    className={"text-3xl font-bold text-center dark:text-white"}>
-                                    Oscar Castro
-                                </div>
-                                <div className={"text-sm text-gray-500 font-semibold"}>
-                                    Full Stack Web Developer
-                                </div>
-                            </div>
-                            <div className="avatar">
-                                <div className="w-48 mask mask-squircle">
-                                    <Image
-                                        alt={"headshot"}
-                                        src={"/images/codeup-final.webp"}
-                                        width={150}
-                                        height={150}
-                                    />
-                                </div>
-                            </div>
-                            <div
-                                className={"flex w-full flex-col items-center"}>
-                                <a className={"link link-white hover:text-primary pb-5 dark:text-white"}
-                                   href={"mailto:oscar.a.castro818@gmail.com"}>oscar.a.castro818@gmail.com</a>
-                                <a aria-label="portfolio" href={"https://oscar-ct.com/"}
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-                                    <CustomBtn customClass={"bg-zinc-700"}>
-                                        Visit Portfolio
-                                    </CustomBtn>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <form method="dialog" className="modal-backdrop">
-                        <button>close</button>
-                    </form>
-                </dialog>
-            </>
-
-
-        )
-            ;
-    }
-    return <Loading/>
-};
 
 export default Page;
