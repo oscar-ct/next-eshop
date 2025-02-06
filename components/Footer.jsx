2e7a40b0780a9df9f0c3a0d52d02d368f7f8940f
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-05 21:36:09 -0600

    update foot size and style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,36 +5,47 @@ import logo from "@/icons/e.svg";
 
 const Footer = () => {
     return (
-        <footer className="px-4 mx-auto max-w-screen-2xl bg-zinc-50 footer py-10 relative 2xl:px-0">
-            <div className={"w-full flex flex-col items-center justify-center gap-4 lg:justify-start lg:items-start"}>
-                <Link href={"/"}>
-                    <Image
-                        width={36}
-                        height={36}
-                        priority
-                        className={"w-[36px] h-auto"}
-                        src={logo}
-                        alt="eshopjs.com"
-                    />
-                </Link>
-                <p className={"footer-title"}>Designed and developed by <a aria-label="portfolio" href={"https://oscar-ct.com/"} target="_blank"
-                                                rel="noopener noreferrer" className={"link"}>Oscar Castro</a></p>
-            </div>
-            <div className={"w-full flex flex-col items-center justify-center lg:justify-start lg:items-start"}>
-                <span className="footer-title">Social</span>
-                <div className="pt-5 grid grid-flow-col gap-4 text-xl">
-                    <a aria-label="linkedin" href={"https://www.linkedin.com/in/oscar-ct"} target="_blank"
-                       rel="noopener noreferrer">
-                        <FaLinkedin/>
-                    </a>
-                    <a aria-label="github" href={"https://github.com/oscar-ct"} target="_blank"
-                       rel="noopener noreferrer">
-                        <FaGithub/>
-                    </a>
-                    <a aria-label="portfolio" href={"https://oscar-ct.com/"} target="_blank"
-                       rel="noopener noreferrer">
-                        <FaGlobe/>
-                    </a>
+        <footer className="bg-zinc-50 h-72 px-4 mx-auto max-w-screen-2xl md:h-56">
+            <div className={"h-full flex flex-col items-center justify-evenly opacity-60 md:flex-row md:pt-10"}>
+                <div className={"h-20 w-full md:w-7/12 lg:w-6/12 flex flex-col items-center justify-between md:items-start"}>
+                    <Link href={"/"}>
+                        <Image
+                            width={36}
+                            height={36}
+                            priority
+                            className={"w-9 h-auto"}
+                            src={logo}
+                            alt="eshopjs.com"
+                        />
+                    </Link>
+                    <div className={"uppercase font-bold flex gap-1"}>
+                        Designed and developed by
+                        <a
+                            aria-label="portfolio"
+                            href={"https://oscar-ct.com/"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={"link"}>
+                            Oscar Castro
+                        </a>
+                    </div>
+                </div>
+                <div className={"h-20 w-full md:w-5/12 lg:w-6/12 flex flex-col justify-between items-center md:items-start"}>
+                    <span className="uppercase font-bold">Social</span>
+                    <div className="grid grid-flow-col gap-4">
+                        <a aria-label="linkedin" href={"https://www.linkedin.com/in/oscar-ct"} target="_blank"
+                           rel="noopener noreferrer">
+                            <FaLinkedin size={22}/>
+                        </a>
+                        <a aria-label="github" href={"https://github.com/oscar-ct"} target="_blank"
+                           rel="noopener noreferrer">
+                            <FaGithub size={22}/>
+                        </a>
+                        <a aria-label="portfolio" href={"https://oscar-ct.com/"} target="_blank"
+                           rel="noopener noreferrer">
+                            <FaGlobe size={22}/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
