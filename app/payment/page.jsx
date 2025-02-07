0ac7895ff1c5e38ceaf1df8e5195b76c23011ca6
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-07 12:05:32 -0600

    update payment page styling to align with new theme

--- app/payment/page.jsx
+++ app/payment/page.jsx
@@ -8,6 +8,7 @@ import stripe from "@/icons/stripe-logo.svg";
 import Image from "next/image";
 import CustomBtn from "@/components/CustomBtn";
 import CheckoutSteps from "@/components/CheckoutSteps";
+import Loading from "@/app/loading";
 
 
 const PaymentPage = () => {
@@ -43,91 +44,83 @@ const PaymentPage = () => {
     if (mounted) return (
         <>
             <CheckoutSteps/>
-            <div className={"pt-0 w-full flex justify-center"}>
-                <div className={"pt-3 sm:pt-7 w-full sm:w-[35em]"}>
-                    <div className={"hidden md:block py-2 text-center text-3xl md:text-2xl font-semibold ibmplex bg-white md:bg-zinc-700 md:text-white"}>
-                        <h1>Select your payment method</h1>
-                    </div>
-                    <h1 className={"md:hidden pt-4 text-center font-semibold text-3xl bg-white px-2"}>
+            <div className={"px-2 w-full flex justify-center sm:pt-10"}>
+                <div className={"bg-zinc-50 z-20 px-4 py-8 w-full rounded-2xl max-w-xl sm:px-8 sm:bg-white sm:shadow-lg sm:border-none"}>
+                    <h1 className={"h-16 flex items-start justify-center font-semibold text-3xl text-center"}>
                         Select your payment method
                     </h1>
-                    <div className={"md:shadow-lg bg-white px-10 pb-5 pt-5 md:border text-sm"}>
-                        <form onSubmit={submitPaymentMethod}>
-                            <div className="my-5">
-                                <div
-                                    className={`w-full border cursor-pointer rounded-md shadow-sm ${paymentMeth === "PayPal / Credit Card" && "ring-2 border-green-500 ring-green-100"}`}
-                                    onClick={() => setPaymentMeth("PayPal / Credit Card")}
-                                >
-                                    <div className={"w-full flex px-6 py-5"}>
-                                        <div className={"w-2/12 flex justify-center"}>
-                                            <Image
-                                                className={"w-[50px] h-[50px]"}
-                                                src={paypal}
-                                                alt={"paypal"}
-                                                width={50}
-                                                height={50}
-                                            />
-                                        </div>
-                                        <div className={"w-9/12 flex items-center"}>
-                                             <span className={"px-3"}>
-                                                PayPal, Venmo, and Credit Card.
-                                            </span>
-                                        </div>
-                                        <div className={"w-1/12 flex items-center"}>
-                                            <input
-                                                onChange={(e) => setPaymentMeth(e.target.value)}
-                                                type="radio"
-                                                name="paymentMethod"
-                                                id={"PayPal / Credit Card"}
-                                                value={"PayPal / Credit Card"}
-                                                className="radio radio-primary"
-                                                checked={paymentMeth === "PayPal / Credit Card"}
-                                            />
-                                        </div>
-                                    </div>
+                    <form onSubmit={submitPaymentMethod} className={"pt-5 space-y-5"}>
+                        <div
+                            className={`w-full border cursor-pointer rounded-md shadow-sm ${paymentMeth === "PayPal / Credit Card" && "ring-2 border-green-500 ring-green-100"}`}
+                            onClick={() => setPaymentMeth("PayPal / Credit Card")}
+                        >
+                            <div className={"w-full flex px-6 py-5"}>
+                                <div className={"w-2/12 flex justify-center"}>
+                                    <Image
+                                        className={"w-[50px] h-[50px]"}
+                                        src={paypal}
+                                        alt={"paypal"}
+                                        width={50}
+                                        height={50}
+                                    />
+                                </div>
+                                <div className={"w-9/12 flex items-center"}>
+                                     <span className={"px-3"}>
+                                        PayPal, Venmo, and Credit Card.
+                                    </span>
+                                </div>
+                                <div className={"w-1/12 flex items-center"}>
+                                    <input
+                                        onChange={(e) => setPaymentMeth(e.target.value)}
+                                        type="radio"
+                                        name="paymentMethod"
+                                        id={"PayPal / Credit Card"}
+                                        value={"PayPal / Credit Card"}
+                                        className="radio radio-primary"
+                                        checked={paymentMeth === "PayPal / Credit Card"}
+                                    />
                                 </div>
                             </div>
-                            <div className="my-5">
-                                <div
-                                    className={`w-full border cursor-pointer rounded-md shadow-sm ${paymentMeth === "Stripe / Credit Card" && "ring-2 border-green-500 ring-green-100"}`}
-                                    onClick={() => setPaymentMeth("Stripe / Credit Card")}
-                                >
-                                    <div className={"w-full flex pr-6 pl-3 py-5"}>
-                                        <div className={"w-2/12 flex justify-center"}>
-                                            <Image
-                                                className={"w-full h-auto"}
-                                                src={stripe}
-                                                alt={"stripe"}
-                                                width={50}
-                                                height={50}
-                                            />
-                                        </div>
-                                        <div className={"w-9/12 flex items-center"}>
-                                            <span className={"px-3 py-4"}>
-                                               Credit Card, Cash App, After Pay, and more.
-                                            </span>
-                                        </div>
-                                        <div className={"w-1/12 flex items-center"}>
-                                            <input
-                                                onChange={(e) => setPaymentMeth(e.target.value)}
-                                                type="radio"
-                                                name="paymentMethod"
-                                                id={"Stripe / Credit Card"}
-                                                value={"Stripe / Credit Card"}
-                                                className="radio radio-primary"
-                                                checked={paymentMeth === "Stripe / Credit Card"}
-                                            />
-                                        </div>
-                                    </div>
+                        </div>
+                        <div
+                            className={`w-full border cursor-pointer rounded-md shadow-sm ${paymentMeth === "Stripe / Credit Card" && "ring-2 border-green-500 ring-green-100"}`}
+                            onClick={() => setPaymentMeth("Stripe / Credit Card")}
+                        >
+                            <div className={"w-full flex pr-6 pl-3 py-5"}>
+                                <div className={"w-2/12 flex justify-center"}>
+                                    <Image
+                                        className={"w-full h-auto"}
+                                        src={stripe}
+                                        alt={"stripe"}
+                                        width={50}
+                                        height={50}
+                                    />
+                                </div>
+                                <div className={"w-9/12 flex items-center"}>
+                                    <span className={"px-3 py-4"}>
+                                       Credit Card, Cash App, After Pay, and more.
+                                    </span>
+                                </div>
+                                <div className={"w-1/12 flex items-center"}>
+                                    <input
+                                        onChange={(e) => setPaymentMeth(e.target.value)}
+                                        type="radio"
+                                        name="paymentMethod"
+                                        id={"Stripe / Credit Card"}
+                                        value={"Stripe / Credit Card"}
+                                        className="radio radio-primary"
+                                        checked={paymentMeth === "Stripe / Credit Card"}
+                                    />
                                 </div>
                             </div>
-                            <div className={"pt-5 w-full flex justify-end"}>
-                                <CustomBtn isDisabled={paymentMeth === null} type={"submit"}>
-                                    Save and Continue
-                                </CustomBtn>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div className={"pt-5 w-full flex justify-end"}>
+                            <CustomBtn isDisabled={paymentMeth === null} type={"submit"}>
+                                Proceed To Checkout
+                            </CustomBtn>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </>
