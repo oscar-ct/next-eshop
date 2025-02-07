d1805c28f9170804ae15eae30a57cf2f23b1e80a
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-07 11:04:08 -0600

    update cart icon, add temp icon for unmounted state, remove props and use direct context

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -1,44 +1,71 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import {useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import CustomBtn from "./CustomBtn";
 import {useRouter} from "next/navigation";
 import {convertCentsToUSD} from "@/utils/covertCentsToUSD";
 import {GrCart} from "react-icons/gr";
+import GlobalContext from "@/context/GlobalContext";
 
 
-const CartIcon = ({ isValidShippingAddress, isValidPaymentMethod, totalCartItems, subtotalPrice }) => {
+const CartIcon = () => {
+
+    const { cartItems, itemsPrice, shippingAddress, paymentMethod } = useContext(GlobalContext);
+
+    const totalCartItems = cartItems.reduce((acc, item) => {
+        return acc + item.quantity
+    }, 0);
 
     const [cartDropdownActive, setCartDropdownActive] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
     const router = useRouter();
 
-    return (
+    useEffect(() => {{
+        if (!isMounted) {
+            setIsMounted(true);
+        }
+    }}, [isMounted]);
+
+    if (!isMounted) return (
+        <Link href={"/cart"} className="px-2 w-6 h-12 flex items-center md:h-16 md:px-0">
+            <div className="indicator">
+                <div className="badge text-white bg-violet-600 badge-sm indicator-item border-none">
+                    <span className="loading loading-bars w-2"/>
+                </div>
+                <GrCart size={24}/>
+            </div>
+        </Link>
+    );
+    if (isMounted) return (
         <>
-            <Link href={"/cart"} className={"px-2 h-12 flex items-center md:hidden"}>
+            <Link href={"/cart"} className={"px-2 w-6 h-12 flex items-center md:hidden"}>
                 <div className="indicator">
-                    <GrCart size={25}/>
                     {
                         totalCartItems !== 0 && (
-                            <span className="badge text-white bg-violet-600 badge-sm indicator-item border-none">{totalCartItems}</span>
+                            <div className="badge text-white bg-violet-600 badge-sm indicator-item border-none">
+                                {totalCartItems}
+                            </div>
                         )
                     }
+                    <GrCart size={24}/>
                 </div>
             </Link>
-
             <div
                 onMouseEnter={() => setCartDropdownActive(true)}
                 onMouseLeave={() => setCartDropdownActive(false)}
                 className={"hidden relative h-16 md:block"}
             >
-                <Link href={"/cart"} className="h-16 flex items-center">
+                <Link href={"/cart"} className="w-6 h-16 flex items-center">
                     <div className="indicator">
-                        <GrCart size={25}/>
                         {
                             totalCartItems !== 0 && (
-                                <span className="badge text-white bg-violet-600 badge-sm indicator-item border-none">{totalCartItems}</span>
+                                <div className="badge text-white bg-violet-600 badge-sm indicator-item border-none">
+                                    {totalCartItems}
+                                </div>
                             )
                         }
+                        <GrCart size={24}/>
                     </div>
                 </Link>
                 {
@@ -48,16 +75,16 @@ const CartIcon = ({ isValidShippingAddress, isValidPaymentMethod, totalCartItems
                                 {
                                     totalCartItems !== 0 ? (
                                         <>
-                                            <span className="text-xl">({totalCartItems}) {totalCartItems > 1 ? "Items" : "Item"}</span>
+                                            <div className="text-xl">({totalCartItems}) {totalCartItems > 1 ? "Items" : "Item"}</div>
                                             <div className="text-info py-3">
-                                                Subtotal:<span className={"pl-2 text-white"}>{convertCentsToUSD(subtotalPrice)}</span>
+                                                Subtotal:<span className={"pl-2 text-white"}>{convertCentsToUSD(itemsPrice)}</span>
                                             </div>
-                                            <CustomBtn customClass={"w-full"} onClick={() => router.push(isValidShippingAddress && isValidPaymentMethod ? "/checkout" : "/cart")}>
-                                                {isValidShippingAddress && isValidPaymentMethod ? "Checkout" : "View Cart"}
+                                            <CustomBtn customClass={"w-full"} onClick={() => router.push(Object.keys(shippingAddress).length !== 0 && paymentMethod !== null ? "/checkout" : "/cart")}>
+                                                {Object.keys(shippingAddress).length !== 0 && paymentMethod !== null ? "Checkout" : "View Cart"}
                                             </CustomBtn>
                                         </>
                                     ) : (
-                                        <span className={"text-center text-xl"}>Your cart is empty...</span>
+                                        <div className={"text-center text-xl"}>Your cart is empty...</div>
                                     )
                                 }
                             </div>
