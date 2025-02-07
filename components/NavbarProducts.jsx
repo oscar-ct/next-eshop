f9198b9587bda9c074a0b8d2a1ebfef9d7599531
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-07 11:51:45 -0600

    create new component for desktop navbar products dropdown menu

---
+++ components/NavbarProducts.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {FaChevronDown} from "react-icons/fa";
+import Link from "next/link";
+import {useState} from "react";
+
+const NavbarProducts = ({ latestProductsLink, topRatedLink }) => {
+
+    const rotateChevron = (action) => {
+        return action ? "open" : "closed";
+    };
+    const [productsDropdownActive, setProductsDropdownActive] = useState(false);
+
+    return (
+        <div className={"relative h-16"}
+             onMouseEnter={() => setProductsDropdownActive(true)}
+             onMouseLeave={() => setProductsDropdownActive(false)}>
+            <div className={"cursor-pointer h-16 flex items-center"}>
+                <h5 className={"font-semibold pr-1.5"}>Shop</h5>
+                <div className={`${rotateChevron(productsDropdownActive)}`}>
+                    <FaChevronDown className={"w-2.5"}/>
+                </div>
+            </div>
+            {
+                productsDropdownActive && (
+                    <div className="absolute right-0 z-10 origin-top-right">
+                        <div className="bg-slate-800/70 rounded-b-md text-white font-bold flex flex-col justify-between w-full">
+                            <div className={"flex-col w-full"}>
+                                <Link href={latestProductsLink}
+                                      className={"block px-10 py-5 hover:bg-white/70 text-white hover:text-black"}
+                                >
+                                    <span className={"w-full text-xl whitespace-nowrap"}>Latest Products</span>
+                                </Link>
+                                <Link href={topRatedLink}
+                                      className={"block px-10 py-5 hover:bg-white/70 text-white hover:text-black"}
+                                >
+                                    <span className={"w-full text-xl whitespace-nowrap"}>Top Rated</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default NavbarProducts;
