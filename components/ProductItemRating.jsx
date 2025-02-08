722d0eeaa92ea78705b08853b613df676317f86a
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-08 16:31:45 -0600

    replace icons, clean up code

--- components/ProductItemRating.jsx
+++ components/ProductItemRating.jsx
@@ -1,26 +1,28 @@
-import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa'
+import {IoStar, IoStarHalfOutline, IoStarOutline} from "react-icons/io5";
 
-const ProductItemRating = ({rating, text = ""} ) => {
+const ProductItemRating = ({rating, text} ) => {
     return (
-        <div className={"flex items-center gap-1"}>
-            <span className={"text-yellow-400"}>
-                {rating >= 1 ? <FaStar/> : rating >= 0.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-            </span>
-            <span className={"text-yellow-400"}>
-                {rating >= 2 ? <FaStar/> : rating >= 1.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-            </span>
-            <span className={"text-yellow-400"}>
-                {rating >= 3 ? <FaStar/> : rating >= 2.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-            </span>
-            <span className={"text-yellow-400"}>
-                {rating >= 4 ? <FaStar/> : rating >= 3.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-            </span>
-            <span className={"text-yellow-400"}>
-                {rating >= 5 ? <FaStar/> : rating >= 4.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-            </span>
-            <span className={"pl-1"}>
-                {text}
-            </span>
+        <div className={"flex items-center gap-1 text-yellow-400"}>
+            <div>
+                {rating >= 1 ? <IoStar/> : rating >= 0.5 ? <IoStarHalfOutline/> : <IoStarOutline/>}
+            </div>
+            <div>
+                {rating >= 2 ? <IoStar/> : rating >= 1.5 ? <IoStarHalfOutline/> : <IoStarOutline/>}
+            </div>
+            <div>
+                {rating >= 3 ? <IoStar/>  : rating >= 2.5 ? <IoStarHalfOutline/> : <IoStarOutline/>}
+            </div>
+            <div>
+                {rating >= 4 ? <IoStar/> : rating >= 3.5 ? <IoStarHalfOutline/> : <IoStarOutline/>}
+            </div>
+            <div>
+                {rating >= 5 ? <IoStar/> : rating >= 4.5 ? <IoStarHalfOutline/> : <IoStarOutline/>}
+            </div>
+            {
+                text && (
+                    <span className={"text-gray-700"}>{text}</span>
+                )
+            }
         </div>
     );
 };
