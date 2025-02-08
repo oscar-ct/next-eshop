02a036accda9318ea5ac00912191b1d15890470e
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-08 02:15:33 -0600

    update login page card styling to align with app refresh

--- app/login/form.jsx
+++ app/login/form.jsx
@@ -108,96 +108,95 @@ const LoginForm = () => {
 
     return (
         <>
-            <div className="h-max relative">
-                <div className="h-full flex flex-row justify-center">
-                    <div className="sm:mt-10 md:mb-10 w-full flex justify-center self-center">
-                        <div className="bg-white border p-12 mx-auto sm:w-96 w-full">
-                            <div className="mb-4">
-                                <h3 className="font-bold text-2xl">Login
-                                </h3>
-                                <p className="text-gray-500">Please sign in to your account
-                                </p>
+            <div className="h-full flex flex-row justify-center px-2 py-10 sm:px-0">
+                <div className="sm:mt-10 md:mb-10 w-full flex justify-center self-center">
+                    <div className="bg-white z-20 px-4 py-8 w-full rounded-2xl sm:w-96 sm:px-8 sm:bg-white sm:shadow-lg sm:border-none">
+                        <div className="mb-4 text-center sm:text-start">
+                            <h3 className="font-bold text-2xl">Login
+                            </h3>
+                            <p className="text-gray-500">Please sign in to your account
+                            </p>
+                        </div>
+                        <form onSubmit={submitLogin} className="space-y-5">
+                            <div className="space-y-2">
+                                <label htmlFor={"email"} className="text-sm font-medium text-gray-600 tracking-wide">Email
+                                </label>
+                                <input
+                                    className="bg-white w-full text-base px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-200 focus:outline-none focus:border-blue-400"
+                                    autoComplete={"email"}
+                                    type={"email"}
+                                    placeholder={"mail@gmail.com"}
+                                    id={"email"}
+                                    value={email}
+                                    onChange={onCredentialChange}
+                                    required
+                                />
                             </div>
-                            <form onSubmit={submitLogin} className="space-y-5">
-                                <div className="space-y-2">
-                                    <label htmlFor={"email"} className="text-sm font-medium text-gray-600 tracking-wide">Email
-                                    </label>
-                                    <input
-                                        className="bg-white w-full text-base px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-200 focus:outline-none focus:border-blue-400"
-                                        autoComplete={"email"}
-                                        type={"email"}
-                                        placeholder={"mail@gmail.com"}
-                                        id={"email"}
-                                        value={email}
-                                        onChange={onCredentialChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="space-y-2">
-                                    <label htmlFor={"password"} className="mb-5 text-sm font-medium text-gray-600 tracking-wide">
-                                        Password
-                                    </label>
+                            <div className="space-y-2">
+                                <label htmlFor={"password"} className="mb-5 text-sm font-medium text-gray-600 tracking-wide">
+                                    Password
+                                </label>
 
-                                    <input
-                                        className="bg-white w-full content-center text-base px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-200 focus:outline-none focus:border-blue-400"
-                                        autoComplete={"off"}
-                                        type={ showPassword ? "text" : "password"}
-                                        placeholder={"Enter your password"}
-                                        id={"password"}
-                                        value={password}
-                                        onChange={onCredentialChange}
-                                        required
-                                    />
-                                    <div className={"flex justify-start flex-row-reverse"}>
-                                        {
-                                            showPassword ? (
-                                                <FaEye
-                                                    onClick={() => setShowPassword(prevState => !prevState)}
-                                                    className={"show-password-img"}
-                                                />
-                                            ) : (
-                                                <FaEyeSlash
-                                                    onClick={() => setShowPassword(prevState => !prevState)}
-                                                    className={"show-password-img"}
-                                                />
-                                            )
-                                        }
+                                <input
+                                    className="bg-white w-full content-center text-base px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-200 focus:outline-none focus:border-blue-400"
+                                    autoComplete={"off"}
+                                    type={ showPassword ? "text" : "password"}
+                                    placeholder={"Enter your password"}
+                                    id={"password"}
+                                    value={password}
+                                    onChange={onCredentialChange}
+                                    required
+                                />
+                                <div className={"flex justify-start flex-row-reverse"}>
+                                    {
+                                        showPassword ? (
+                                            <FaEye
+                                                onClick={() => setShowPassword(prevState => !prevState)}
+                                                className={"show-password-img"}
+                                            />
+                                        ) : (
+                                            <FaEyeSlash
+                                                onClick={() => setShowPassword(prevState => !prevState)}
+                                                className={"show-password-img"}
+                                            />
+                                        )
+                                    }
 
-                                        <div className="flex justify-end">
-                                            <div className="text-sm">
-                                                <button
-                                                    type={"button"}
-                                                    onClick={(e) => {
-                                                        e.preventDefault()
-                                                        window.password_modal.showModal()
-                                                    }
-                                                    }
-                                                    className="text-blue-400 hover:text-blue-500">
-                                                    Forgot your password?
-                                                </button>
-                                            </div>
+                                    <div className="flex justify-end">
+                                        <div className="text-sm">
+                                            <button
+                                                type={"button"}
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    window.password_modal.showModal()
+                                                }
+                                                }
+                                                className="text-blue-400 hover:text-blue-500">
+                                                Forgot your password?
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
-
-                                <div className={"flex justify-center"}>
-                                    <button type="submit" className="rounded-full btn btn-neutral btn-wide normal-case ibmplex text-base">
-                                        Login
-                                    </button>
-                                </div>
-                            </form>
-                            <div className={"flex justify-center items-center text-sm"}>
-                                <p className={"pt-6"}>
-                                    Don&apos;t have an account?
-                                </p>
-                                <Link href={"/register"} className="link text-blue-400 hover:text-blue-500 pt-6 pl-1">
-                                    Create new account
-                                </Link>
                             </div>
+
+                            <div className={"flex justify-center"}>
+                                <button type="submit" className="rounded-full btn btn-neutral btn-wide normal-case ibmplex text-base">
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                        <div className={"flex justify-center items-center text-sm"}>
+                            <p className={"pt-6"}>
+                                Don&apos;t have an account?
+                            </p>
+                            <Link href={"/register"} className="link text-blue-400 hover:text-blue-500 pt-6 pl-1">
+                                Create new account
+                            </Link>
                         </div>
                     </div>
                 </div>
             </div>
+
             <dialog id="password_modal" className="modal modal-bottom sm:modal-middle">
                 <form method="dialog" className="modal-box bg-white">
                     <div className={"flex justify-between items-center"}>
@@ -233,7 +232,6 @@ const LoginForm = () => {
             </dialog>
         </>
     )
-
 };
 
 export default LoginForm;
