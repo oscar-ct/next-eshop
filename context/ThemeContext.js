01e40e4d6ac5328613edc66653136ad670e4a101
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-10 17:57:09 -0600

    remove theme context/reducer in favor of next theme package

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,29 +1,29 @@
-"use client";
-
-import {createContext, useReducer} from "react";
-import themeReducer from "@/context/ThemeReducer";
-
-const ThemeContext = createContext();
-
-export function ThemeProvider({ children }) {
-
-    const initialState = {
-        isDarkMode: typeof window !== "undefined" && !window.matchMedia('(prefers-color-scheme: light)').matches,
-        isInitialRender: true,
-    };
-
-    const [state, dispatch] = useReducer(themeReducer, initialState);
-
-
-    return (
-        <ThemeContext.Provider value={{
-            dispatch,
-            isDarkMode: state.isDarkMode,
-            isInitialRender: state.isInitialRender,
-        }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-}
-
-export default ThemeContext;
+// "use client";
+//
+// import {createContext, useReducer} from "react";
+// import themeReducer from "@/context/ThemeReducer";
+//
+// const ThemeContext = createContext();
+//
+// export function ThemeProvider({ children }) {
+//
+//     const initialState = {
+//         isDarkMode: typeof window !== "undefined" && !window.matchMedia('(prefers-color-scheme: light)').matches,
+//         isInitialRender: true,
+//     };
+//
+//     const [state, dispatch] = useReducer(themeReducer, initialState);
+//
+//
+//     return (
+//         <ThemeContext.Provider value={{
+//             dispatch,
+//             isDarkMode: state.isDarkMode,
+//             isInitialRender: state.isInitialRender,
+//         }}>
+//             {children}
+//         </ThemeContext.Provider>
+//     )
+// }
+//
+// export default ThemeContext;
--- context/ThemeReducer.js
+++ context/ThemeReducer.js
@@ -1,19 +1,19 @@
-const themeReducer = (state, action) => {
-    switch (action.type) {
-        case "TOGGLE_THEME":
-            return {
-                ...state,
-                isDarkMode: !state.isDarkMode,
-            }
-        case "RENDER_FALSE":
-            return {
-                ...state,
-                isInitialRender: false,
-            }
-        default:
-            console.log("case missed...")
-            return state;
-    }
-};
-
-export default themeReducer;
+// const themeReducer = (state, action) => {
+//     switch (action.type) {
+//         case "TOGGLE_THEME":
+//             return {
+//                 ...state,
+//                 isDarkMode: !state.isDarkMode,
+//             }
+//         case "RENDER_FALSE":
+//             return {
+//                 ...state,
+//                 isInitialRender: false,
+//             }
+//         default:
+//             console.log("case missed...")
+//             return state;
+//     }
+// };
+//
+// export default themeReducer;
