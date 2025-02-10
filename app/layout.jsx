b5c5f723a060efa1fa934d5980a5256dfddf1892
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-10 17:57:47 -0600

    add theme provider to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import AuthProvider from "@/components/AuthProvider";
 import {GlobalProvider} from "@/context/GlobalContext";
 import Footer from "@/components/Footer";
 import AddToCartModal from "@/components/modals/AddToCartModal";
-import {ThemeProvider} from "@/context/ThemeContext";
+import Theme from "@/app/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,20 +19,21 @@ export default async function RootLayout({children}) {
     return (
         <GlobalProvider>
             <AuthProvider>
-                <ThemeProvider>
-                <html lang="en">
-                <body className={`${inter.className} bg-zinc-50 dark:bg-slate-800`}>
-                <Toaster/>
-                <Navbar/>
-                <main className={"mt-12 mx-auto min-h-[calc(100vh-336px)] md:mt-16 md:min-h-[calc(100vh-288px)] relative overflow-x-clip overflow-y-hidden"}>
-                    {children}
-                    <div className={"z-0 scale-125 -rotate-6 m-auto fixed w-full top-72 h-72 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"}/>
-                </main>
-                <AddToCartModal/>
-                <Footer/>
-                </body>
+                <html lang="en" suppressHydrationWarning>
+                    <body className={`${inter.className} bg-zinc-50 dark:bg-slate-800`}>
+                        <Theme>
+                            <Toaster/>
+                            <Navbar/>
+                            <main className={"mt-12 mx-auto min-h-[calc(100vh-336px)] md:mt-16 md:min-h-[calc(100vh-288px)] relative overflow-x-clip overflow-y-hidden"}>
+                                {children}
+                                <div className={"z-0 scale-125 -rotate-6 m-auto fixed w-full top-72 h-72 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"}/>
+                            </main>
+                            <AddToCartModal/>
+                            <Footer/>
+                        </Theme>
+                    </body>
+
                 </html>
-                </ThemeProvider>
             </AuthProvider>
         </GlobalProvider>
     );
