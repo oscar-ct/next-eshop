d2c7f4ec26439fa6534a16f194ef1dc2e5b1a1df
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-10 17:58:07 -0600

    create theme provider for root layout

---
+++ app/theme.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import {ThemeProvider} from "next-themes";
+
+const Theme = ({children}) => {
+    return (
+        <ThemeProvider attribute={"class"}>
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default Theme;
