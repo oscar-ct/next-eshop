473983974d83d975f98357b24792ba38363c57bd
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-11 16:41:26 -0600

    convert main app home page to server component, call server session status to pass down to children props

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,15 @@
-"use client"
-
 import HomePageProducts from "@/components/HomePageProducts";
 import HomePageIntro from "@/components/HomePageIntro";
 import OscarModal from "@/components/modals/OscarModal";
+import {getServerSession} from "next-auth";
 
-
-const Page = () => {
-
+const Page = async () => {
+    const session = await getServerSession();
     return (
         <>
             <div className={"relative overflow-hidden px-2 lg:px-4 2xl:px-0"}>
                 <div className={"z-10 m-auto max-w-screen-2xl flex flex-col relative gap-4 sm:gap-8"}>
-                    <HomePageIntro/>
+                    <HomePageIntro session={session} />
                     {/*HomePageIntro includes: swiper slide, product categories*/}
                     <HomePageProducts/>
                 </div>
