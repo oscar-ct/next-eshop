bddfabfc3e4c34cf83ee62f157987d13bb806e2d
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-12 06:04:27 -0600

    revamp products sort and filter page, add skeleton dummy cards and dummy select menus while loading to increase page visuals

--- app/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/page.jsx
+++ app/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/page.jsx
@@ -11,8 +11,9 @@ import ProductItem from "@/components/ProductItem";
 import Message from "@/components/Message";
 import Paginate from "@/components/Paginate";
 import toast from "react-hot-toast";
-import Loading from "@/app/loading";
-
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+import ProductItemSkeletons from "@/components/ProductItemSkeletons";
+import {IoIosArrowDown} from "react-icons/io";
 
 const fetchProducts = async (params) => {
     const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
@@ -36,12 +37,10 @@ const fetchProducts = async (params) => {
 const ProductsPage = () => {
 
     const params = useParams();
-    const { sortTerm, categoryTerm, pageNumber } = params;
+    const {sortTerm, categoryTerm, pageNumber} = params;
 
     const [products, setProducts] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [windowInnerWidth, setWindowInnerWidth] = useState(typeof window !== "undefined" && window.innerWidth);
-
 
     useEffect(() => {
         const fetchProductsData = async () => {
@@ -59,70 +58,121 @@ const ProductsPage = () => {
         }
     }, [products]);
 
-    useEffect( () => {
-        const adjustWidth = () => {
-            setWindowInnerWidth(window.innerWidth);
-        }
-        window.addEventListener("resize", adjustWidth);
-        return () => window.removeEventListener("resize", adjustWidth);
-    });
+    const {width} = useWindowDimensions();
 
-
-    if (!loading && products) {
-        return (
-            <>
-                <div className={"flex w-full"}>
-                    <div className={"w-2/12 md:w-1/12"}>
-                        <BackButton/>
-                    </div>
-                    <BackButtonMessage width={"w-10/12 md:w-11/12"}/>
+    return (
+        <>
+            <div className={"flex w-full"}>
+                <div className={"w-2/12 md:w-1/12"}>
+                    <BackButton/>
                 </div>
-                <div className={"pt-20 lg:pt-14"}>
-                    <div className={"flex justify-between"}>
-                        <div className={"w-full p-1 flex items-center"}>
-                            <div className={"flex w-full"}>
-                                <div className={"flex flex-col justify-between items-center w-6/12"}>
-                                    <p className={"p-1 text-sm dark:text-white"}>Category</p>
-                                    <div className={"px-2 w-full lg:w-96"}>
-                                        <SelectMenuCategory sortTerm={sortTerm} categoryTerm={categoryTerm} pageNumber={pageNumber} customStyles={customStyles}/>
-                                    </div>
+                <BackButtonMessage width={"w-10/12 md:w-11/12"}/>
+            </div>
+            <div className={"pt-20 lg:pt-14"}>
+                <div className={"flex justify-between"}>
+                    <div className={"w-full p-1 flex items-center"}>
+                        <div className={"flex w-full"}>
+                            <div className={"flex flex-col justify-between items-center w-6/12"}>
+                                <p className={"z-10 p-1 text-sm dark:text-white"}>Category</p>
+                                <div className={"px-2 w-full lg:w-96"}>
+                                    {
+                                        loading && !products ? (
+                                            <div className={"z-10"}>
+                                                <div
+                                                    className={"h-[42px] bg-zinc-50 border flex justify-between items-center px-3 py-2.5 text-gray-400"}>
+                                                    <span className="z-30 loading loading-bars loading-sm"/>
+                                                    <div className={"flex h-full items-center gap-2"}>
+                                                        <span className={"w-[1px] h-full bg-gray-300"}></span>
+                                                        <IoIosArrowDown size={17}/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <SelectMenuCategory
+                                                sortTerm={sortTerm}
+                                                categoryTerm={categoryTerm}
+                                                pageNumber={pageNumber}
+                                                customStyles={customStyles}
+                                            />
+                                        )
+                                    }
                                 </div>
-                                <div className={"flex flex-col justify-between items-center w-6/12"}>
-                                    <p className={"p-1 text-sm dark:text-white"}>Sort By</p>
-                                    <div className={"px-2 w-full lg:w-96"}>
-                                        <SelectMenuSort sortTerm={sortTerm} categoryTerm={categoryTerm} pageNumber={pageNumber} customStyles={customStyles}/>
-                                    </div>
+                            </div>
+                            <div className={"flex flex-col justify-between items-center w-6/12"}>
+                                <p className={"z-10 p-1 text-sm dark:text-white"}>Sort By</p>
+                                <div className={"z-10 px-2 w-full lg:w-96"}>
+                                    {
+                                        loading && !products ? (
+                                            <div className={"z-10"}>
+                                                <div
+                                                    className={"w-full h-[42px] bg-zinc-50 border flex justify-between items-center px-3 py-2.5 text-gray-400"}>
+                                                    <span className="z-30 loading loading-bars loading-sm"/>
+                                                    <div className={"flex h-full items-center gap-2"}>
+                                                        <span className={"w-[1px] h-full bg-gray-300"}></span>
+                                                        <IoIosArrowDown size={17}/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <SelectMenuSort
+                                                sortTerm={sortTerm}
+                                                categoryTerm={categoryTerm}
+                                                pageNumber={pageNumber}
+                                                customStyles={customStyles}
+                                            />
+
+                                        )
+                                    }
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={"w-full flex flex-wrap justify-center pt-6"}>
-                    {
-                        products.products.length !== 0 ? (
-                            products.products.map(function (product) {
-                                return (
-                                    <ProductItem key={product.id} product={product}
-                                                 windowInnerWidth={windowInnerWidth}/>
+            </div>
+            {
+                loading && !products ? (
+                    <ProductItemSkeletons/>
+                ) : (
+                    <>
+
+                        <div className={"w-full flex flex-wrap justify-center pt-6"}>
+                            {
+                                products.products.length !== 0 ? (
+                                    products.products.map(function (product) {
+                                        return (
+                                            <ProductItem
+                                                key={product.id}
+                                                product={product}
+                                                windowInnerWidth={width}
+                                            />
+                                        )
+                                    })
+                                ) : (
+                                    <div className={"z-10 p-4"}>
+                                        <Message variant={"warning"}>
+                                            It appears we dont have what you are looking for at the moment :(
+                                        </Message>
+                                    </div>
                                 )
-                            })
-                        ) : (
-                            <div className={"p-4"}>
-                                <Message variant={"warning"}>It appears we dont have what you are looking for at the
-                                    moment :(</Message>
+                            }
+                        </div>
+                        <div className={"pt-10 flex justify-center"}>
+                            <div className={"join"}>
+                                <Paginate
+                                    pages={products.pages}
+                                    page={products.page}
+                                    sortTerm={sortTerm}
+                                    categoryTerm={categoryTerm}
+                                />
                             </div>
-                        )
-                    }
-                </div>
-                <div className={"pt-10 flex justify-center"}>
-                    <div className={"join"}>
-                        <Paginate pages={products.pages} page={products.page} sortTerm={sortTerm} categoryTerm={categoryTerm} />
-                    </div>
-                </div>
-            </>
-        );
-    }
-    return <Loading/>
+                        </div>
+                    </>
+                )
+            }
+      </>
+    );
+
+
 };
 
 export default ProductsPage;
