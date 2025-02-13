f358497a2115dcd5ec265da5a1492230003d0b08
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-12 19:53:37 -0600

    add unmounted checkout steps on page load while functional steps successfully mounts

--- components/CheckoutSteps.jsx
+++ components/CheckoutSteps.jsx
@@ -31,70 +31,102 @@ const CheckoutSteps = () => {
     const isPaymentPage = pathname.includes(step3name.toLowerCase());
     const isPlaceOrderPage = pathname.includes(step4name.toLowerCase());
 
-    if (mounted) {
-        return (
+    return (
+        !mounted ? (
+            <div className={"flex justify-center pt-5 text-xs sm:text-sm font-semibold"}>
+                <div className={"steps w-full"}>
+                    <button data-content="?"
+                            className={`cursor-default step ${isCartPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                        {step1name}
+                    </button>
+                    <button data-content="?"
+                            className={`cursor-default step ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                        {step2name}
+                    </button>
+                    <button data-content="?"
+                            className={`cursor-default step ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                        {step3name}
+                    </button>
+                    <button data-content="?"
+                            className={`cursor-default step ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                        {step4name}
+                    </button>
+                </div>
+            </div>
+        ) : (
             <div className={"flex justify-center pt-5 text-xs sm:text-sm font-semibold"}>
                 <div className={"steps w-full"}>
                     {
                         cartItems.length !== 0 ? (
-                            <Link href={"/cart"} data-content="✓" className={`step step-success ${isCartPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link href={"/cart"} data-content="✓"
+                                  className={`step step-success ${isCartPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step1name}
                             </Link>
                         ) : (
-                            <button data-content="✕" className={`cursor-default step ${isCartPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <button data-content="✕"
+                                    className={`cursor-default step ${isCartPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step1name}
                             </button>
                         )
                     }
                     {
                         cartItems.length !== 0 && Object.keys(shippingAddress).length !== 0 ? (
-                            <Link data-content="✓" href={"/shipping"} className={`step step-success ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✓" href={"/shipping"}
+                                  className={`step step-success ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step2name}
                             </Link>
                         ) : cartItems.length !== 0 && Object.keys(shippingAddress).length === 0 ? (
-                            <Link data-content="✕" href={"/shipping"} className={`step ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✕" href={"/shipping"}
+                                  className={`step ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step2name}
                             </Link>
                         ) : (
-                            <button data-content="✕" className={`cursor-default step ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <button data-content="✕"
+                                    className={`cursor-default step ${isShippingPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step2name}
                             </button>
                         )
                     }
                     {
                         !paymentMethod && Object.keys(shippingAddress).length !== 0 ? (
-                            <Link data-content="✕" href={"/payment"} className={`step ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✕" href={"/payment"}
+                                  className={`step ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step3name}
                             </Link>
                         ) : paymentMethod ? (
-                            <Link data-content="✓" href={"/payment"} className={`step step-success ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✓" href={"/payment"}
+                                  className={`step step-success ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step3name}
                             </Link>
                         ) : (
-                            <button data-content="✕" className={`cursor-default step ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <button data-content="✕"
+                                    className={`cursor-default step ${isPaymentPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step3name}
                             </button>
                         )
                     }
                     {
                         paymentMethod && Object.keys(shippingAddress).length !== 0 && !clientSecretExists ? (
-                            <Link data-content="✕" href={"/checkout"} className={`step  ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✕" href={"/checkout"}
+                                  className={`step  ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step4name}
                             </Link>
                         ) : paymentMethod && Object.keys(shippingAddress).length !== 0 && clientSecretExists ? (
-                            <Link data-content="✓" href={"/checkout"} className={`step step-success ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                            <Link data-content="✓" href={"/checkout"}
+                                  className={`step step-success ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step4name}
                             </Link>
-                            ) : (
-                            <button data-content="✕" className={`cursor-default step ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
+                        ) : (
+                            <button data-content="✕"
+                                    className={`cursor-default step ${isPlaceOrderPage ? "text-lg sm:text-xl dark:text-white" : "text-gray-400"}`}>
                                 {step4name}
                             </button>
                         )
                     }
                 </div>
             </div>
-        );
-    }
+        )
+    );
 };
 
 export default CheckoutSteps;
