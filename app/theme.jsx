04243eb60bf362c49073eeabbd869c1d04433568
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-13 05:52:59 -0600

    set to theme default to light

--- app/theme.jsx
+++ app/theme.jsx
@@ -4,7 +4,7 @@ import {ThemeProvider} from "next-themes";
 
 const Theme = ({children}) => {
     return (
-        <ThemeProvider attribute={"class"}>
+        <ThemeProvider attribute={"class"} defaultTheme={"light"}>
             {children}
         </ThemeProvider>
     );
