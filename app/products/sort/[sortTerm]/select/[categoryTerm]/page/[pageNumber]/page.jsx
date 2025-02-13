a549a2f299ef0d26995765ec53ca828b3724b1df
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-13 05:54:09 -0600

    testing fix for z index on mobile

--- app/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/page.jsx
+++ app/products/sort/[sortTerm]/select/[categoryTerm]/page/[pageNumber]/page.jsx
@@ -73,11 +73,11 @@ const ProductsPage = () => {
                     <div className={"w-full p-1 flex items-center"}>
                         <div className={"flex w-full"}>
                             <div className={"flex flex-col justify-between items-center w-6/12"}>
-                                <p className={"z-10 p-1 text-sm dark:text-white"}>Category</p>
+                                <p className={"z-20 flex p-1 text-sm dark:text-white"}>Category</p>
                                 <div className={"flex px-2 w-full lg:w-96"}>
                                     {
                                         loading && !products ? (
-                                            <div className={"w-full z-10"}>
+                                            <div className={"w-full z-20"}>
                                                 <div
                                                     className={"h-[42px] bg-zinc-50 border flex justify-between items-center px-3 py-2.5 text-gray-400"}>
                                                     <span className="z-30 loading loading-bars loading-sm"/>
@@ -99,11 +99,11 @@ const ProductsPage = () => {
                                 </div>
                             </div>
                             <div className={"flex flex-col justify-between items-center w-6/12"}>
-                                <p className={"z-10 p-1 text-sm dark:text-white"}>Sort By</p>
+                                <p className={"z-20 flex p-1 text-sm dark:text-white"}>Sort By</p>
                                 <div className={"flex px-2 w-full lg:w-96"}>
                                     {
                                         loading && !products ? (
-                                            <div className={"w-full z-10"}>
+                                            <div className={"w-full z-20"}>
                                                 <div
                                                     className={"w-full h-[42px] bg-zinc-50 border flex justify-between items-center px-3 py-2.5 text-gray-400"}>
                                                     <span className="z-30 loading loading-bars loading-sm"/>
