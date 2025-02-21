f3a77d3990f4f35016f9c4d56185516b37fea50b
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-20 23:07:56 -0600

    update stripe theme to match current app theme

--- components/StripeCheckout.jsx
+++ components/StripeCheckout.jsx
@@ -4,14 +4,21 @@ import {Elements} from "@stripe/react-stripe-js";
 import StripeCheckoutForm from "./StripeCheckoutForm";
 import {useState} from "react";
 import {loadStripe} from "@stripe/stripe-js/pure";
+import {useTheme} from "next-themes";
 
 
 const StripeCheckout = ({ existingOrder , setSaveButtonDisabled, setOrder }) => {
 
     const [stripePromise] = useState(() => loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_PUBLISHABLE_KEY));
-
+    const { theme} = useTheme();
     const appearance = {
-        theme: 'stripe',
+        theme: theme === "dark" ? "night" : 'stripe',
+        // variables: {
+            // fontFamily: 'Ideal Sans, system-ui, sans-serif',
+            // spacingUnit: '3px',
+            // borderRadius: '4px',
+            // gridRowSpacing: "4px"
+        // }
     };
     const options = {
         mode: 'payment',
