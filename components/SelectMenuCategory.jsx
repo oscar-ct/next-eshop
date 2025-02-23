e73d1875278083bd9e3a1269a9c1d52cc0f5434a
Oscar Castro <oscar.a.castro818@gmail.com>
2025-02-23 00:52:30 -0600

    add options prop to component, remove placeholder function, import helper function to display placeholder

--- components/SelectMenuCategory.jsx
+++ components/SelectMenuCategory.jsx
@@ -1,47 +1,11 @@
 import Select from "react-select";
 import {useRouter} from "next/navigation";
+import capitalizeFirstChar from "@/utils/capitalizeFirstChar";
 
-const SelectMenuCategory = ({sortTerm, categoryTerm, pageNumber, customStyles}) => {
+const SelectMenuCategory = ({sortTerm, categoryTerm, pageNumber, customStyles, options}) => {
 
     const router = useRouter();
 
-    const options = [
-        {value: "all", label: "All"},
-        {value: "apparel", label: "Apparel"},
-        {value: "automotive", label: "Automotive"},
-        {value: "electronics", label: "Electronics"},
-        {value: "food", label: "Food"},
-        {value: "footwear", label: "Footwear"},
-        {value: "games", label: "Games"},
-        {value: "health", label: "Health"},
-        {value: "home", label: "Home"},
-        {value: "pet", label: "Pet"},
-        {value: "other", label: "Other"},
-    ];
-    const placeHolder = (category) => {
-        switch (category) {
-            case "all":
-                return "All"
-            case "apparel":
-                return "Apparel"
-            case "automotive":
-                return "Automotive"
-            case "electronics":
-                return "Electronics"
-            case "food":
-                return "Food"
-            case "footwear":
-                return "Footwear"
-            case "games":
-                return "Games"
-            case "home":
-                return "Home"
-            case "pet":
-                return "Pet"
-            case "other":
-                return "Other"
-        }
-    };
     const handleChange = (selectedOption) => {
         router.push(`/products/sort/${sortTerm}/select/${selectedOption.value}/page/${pageNumber}`);
     };
@@ -50,7 +14,7 @@ const SelectMenuCategory = ({sortTerm, categoryTerm, pageNumber, customStyles}) 
             styles={customStyles}
             isSearchable={false}
             className={"w-full"}
-            placeholder={placeHolder(categoryTerm)}
+            placeholder={capitalizeFirstChar(categoryTerm)}
             options={options}
             onChange={handleChange}
         />
