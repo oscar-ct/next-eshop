600f22b27c559bda5b52289dc1991d5c96680eb4
Oscar Castro <oscar.a.castro818@gmail.com>
2025-03-18 16:28:41 -0500

    clean up component to make it more concise, maintainable, and readable.

---
+++ components/AlertMessage.jsx
@@ -0,0 +1,86 @@
+const AlertMessage = ({ variant = "info", children, border = "" }) => {
+    const icons = {
+        info: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+        ),
+        success: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+        ),
+        warning: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+            </svg>
+        ),
+        error: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+        ),
+    };
+
+    const variantClasses = {
+        info: "alert-info bg-indigo-200",
+        success: "alert-success bg-green-200",
+        warning: "alert-warning",
+        error: "alert-error",
+        default: "",
+    };
+
+    const alertClass = `alert flex border-none opacity-90 !rounded-sm ${variantClasses[variant] || variantClasses.default} ${border}`;
+
+    return (
+        <div className={alertClass}>
+            <div className="flex items-center justify-start gap-1">
+                {icons[variant] || icons.info}
+                <span className="text-start">{children}</span>
+            </div>
+        </div>
+    );
+};
+
+
+export default AlertMessage;
