295cd7c83f2d000fd760cdbafb79f6028a0d98e5
Oscar Castro <oscar.a.castro818@gmail.com>
2025-07-09 19:24:25 -0500

    feat: update homepage titles

--- components/home/HomePageSwiper.jsx
+++ components/home/HomePageSwiper.jsx
@@ -4,7 +4,6 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade';
 import HomePageCreditCards from "@/components/home/HomePageCreditCards";
-import {SiAfterpay, SiCashapp} from "react-icons/si";
 
 const HomePageSwiper = () => {
 
@@ -28,7 +27,7 @@ const HomePageSwiper = () => {
                     <div className={"flex flex-col w-full h-36 md:h-24 relative rainbow"}>
                         <div className={"px-4 flex items-center bg-zinc-50 rounded-lg absolute top-0 left-0 right-0 bottom-0 m-auto h-[calc(100%-4px)] w-[calc(100%-4px)] dark:text-white dark:bg-slate-700"}>
                             <h3 className={"mx-auto text-center sm:text-lg"}>
-                                shoposcar.com is an online e-commerce store built and designed from the ground up by <span
+                                ShopOscar.com is a custom-built e-commerce platform, meticulously designed and developed from the ground up by <span
                                 className={"font-bold text-violet-700 cursor-pointer"} onClick={openModal}>Oscar Castro</span>,
                                 a Web Developer based in San Antonio, Texas.
                             </h3>
@@ -48,15 +47,16 @@ const HomePageSwiper = () => {
                     <div className={"flex flex-col w-full h-36 md:h-24 relative rainbow"}>
                         <div className={"px-4 flex items-center bg-zinc-50 rounded-lg absolute top-0 left-0 right-0 bottom-0 m-auto h-[calc(100%-4px)] w-[calc(100%-4px)] focus:outline-none focus:ring-4 focus:ring-offset-1 dark:text-white dark:bg-slate-700"}>
                             <h3 className={"mx-auto text-center sm:text-lg"}>
-                                shoposcar.com uses <span className={"font-semibold text-blue-600"}>Stripe</span> and <span
+                                ShopOscar.com uses <span className={"font-semibold text-blue-600"}>Stripe</span> and <span
                                 className={"font-bold text-blue-600"}>PayPal</span> to handle credit card transactions, and we
                                 do not store any credit card information.
                             </h3>
                         </div>
                     </div>
                     <div className={"flex flex-col w-full justify-evenly items-center gap-4 lg:flex-row"}>
-                        <p className={"flex items-center justify-center gap-1 flex-wrap text-center py-5 text-xl text-black font-bold dark:text-white"}>
-                            CashApp  <SiCashapp fill={"#58DA14"}/> & Afterpay <SiAfterpay fill={"#9FF5D6"}/> available at checkout
+                        <p className={"text-center py-5 text-xl text-black font-bold flex flex-col lg:text-start dark:text-white"}>
+                            CashApp & Afterpay services available at checkout.
+                            <span className={"text-center text-gray-500 text-sm font-light"}>when use select Stripe at payment</span>
                         </p>
                         <HomePageCreditCards/>
                     </div>
